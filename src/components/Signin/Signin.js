506493d7a6a602cbcb88ccb7902a3cc6f1dc5105
Elena Gregov <124687938+ElenaGregov@users.noreply.github.com>
2023-05-10 13:33:50 +0200

    link

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://faceappbackend-b4p3.onrender.com//signin', {
+    fetch('https://faceappbackend-b4p3.onrender.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
